46134a6fc59d9d24b1f005313a94172ab529ad1a
michael020919 <tm39@illinois.edu>
2025-09-06 18:27:42 -0500

    Update squares.py

--- squares.py
+++ squares.py
@@ -2,11 +2,11 @@ import numpy as np
 import torch
 
 def numpy_squares(k):
-    output = np.arrange(1,k+1)
+    output = np.arange(1,k+1)
     return output ** 2
     # your code here
 
 def torch_squares(k):
-    output = torch.arrange(1,k+1)
+    output = torch.arange(1,k+1)
     return output ** 2
     # your code here
